88c7f7c6a7ab05cc0761c4c960005d4152fb9dd9
anthony <thestonesserver@gmail.com>
2021-01-01 20:31:46 +0000

    Sharable content

--- js/data.js
+++ js/data.js
@@ -7,37 +7,37 @@
 // How to use, fill the data with persons names, youtube video identifiers, and a unique key
 const data = [
     {
-        key: 'NDg5MA==', // 4890
+        key: 'NDg5MA==', // 
         name: 'QXVudCBLYXk=',
         video: 'V2dUNnZXcUdybHM='
     },
     {
-        key: 'NzI5MQ==', // 7291
+        key: 'NzI5MQ==', // 
         name: 'TWF0dA==',
         video: 'am5MVENZaV96ZmM='
     },
     {
-        key: 'MzM4Mg==', // 3382
+        key: 'MzM4Mg==', // 
         name: 'QXVudCBLZXJyeQ==',
         video: 'TmhoclVaWTdsVjA='
     },
     {
-        key: 'MDIxOQ==', // 0219
+        key: 'MDIxOQ==', // 
         name: 'QXVudCBKYW5lICYgVW5jbGUgRGljaw==',
         video: 'cnNzVVhaWW02dU0='
     },
     {
-        key: 'NTM1OQ==', // 5359
+        key: 'NTM1OQ==', // 
         name: 'QXVudCBKdWxpZQ==',
         video: 'X1BwM0VxRUd2QjA='
     },
     {
-        key: 'ODIzOQ==', // 8239
+        key: 'ODIzOQ==', // 
         name: 'S2F0aWUgJiBCcmlhbg==',
         video: 'Xzg5Q2E3REw4em8='
     },
     {
-        key: 'MzkxOA==', // 3918
+        key: 'MzkxOA==', // 
         name: 'RGFk',
         video: 'RnRWa0VOU1YycFU='
     }
